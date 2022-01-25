907e4a98fe31a410bb8a8488319f79bddbee996d
001kanjana <kanjana.mindmind@gmail.com>
2022-01-25 13:38:43 +0700

    v1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>:: สวัสดี ::</title>
+</head>
+
+<body>
+<?php
+ print " ยินดีต้อนรับ<br /> ";
+ print 2+3;
+ $a = 3; $b = 5;
+ print "<br />";
+ print $a + $b;
+ print "<br />";
+ printf ("$a x $b = %d" , $a*$b);
+ print "<br />";
+ printf ("$a / $b = %.2f" , $a/$b);
+?>
+ผลลัพธ์
+
+</body>
+
+</html>
